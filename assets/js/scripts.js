05c5e367d99643761143f8591b1eb1ef686cec28
joaavila <joao.avila@ag2nurun.com.br>
2015-07-03 17:53:59 -0300

    adicionando mais um indice no array

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,22 +4,26 @@ app.controller('controlador', ['$scope', function($scope) {
      		{
 				number: 1,
 				status: 'active',
-				cod: 10	
+				cod: 10,
+				members: ["Mark", 18, "BR"]	
 			},
 			{
 				number: 2,
 				status: 'undefined',
-				cod: 11	
+				cod: 11,
+				members: ["Robert", 22, "BR"]	
 			},
 			{
 				number: 3,
 				status: 'visited',
-				cod: 13	
+				cod: 13,
+				members: ["Carlos", 33, "BR"]	
 			},
 			{
 				number: 4,
 				status: 'testado',
-				cod: 14	
+				cod: 14,
+				members: ["Jhon", 15, "BR"]	
 			}
      	];  
 }]);
